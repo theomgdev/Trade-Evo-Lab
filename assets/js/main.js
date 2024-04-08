6a7054df5c20686318b24a2c86e0d00280ca9fa5
theomgdev <trincitasdad@gmail.com>
2024-04-08 13:11:36 +0300

    better style, dynamic size

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,20 @@
 //scale evo-chart canvas to window size
 var canvas = document.getElementById("evo-chart");
 
+//OHLC (time, open, high, low, close) data
 var evoChart = new EvoChart("evo-chart", [
-    //for each TOHLC (time, open, high, low, close) data
-    [0, 10, 20, 5, 15],
-    [1, 15, 25, 10, 20],
-    [2, 20, 30, 15, 25],
-    [3, 25, 35, 20, 30],
-    //fall
-    [4, 30, 40, 20, 15],
-    [5, 15, 35, 5, 10],
-    [6, 10, 30, 5, 20],
-    [7, 20, 40, 15, 30],
+    [0, 100, 200, 50, 150],
+    [1, 150, 250, 100, 200],
+    [2, 200, 300, 150, 250],
+    [3, 250, 350, 200, 300],
+    [4, 300, 400, 200, 250],
+    [5, 250, 300, 200, 225],
+    [6, 225, 275, 100, 150],
+    [7, 150, 200, 100, 175],
+    [8, 175, 225, 150, 200],
+    [9, 200, 250, 150, 225],
+    [10, 225, 275, 200, 250],
+    [11, 250, 300, 200, 275],
 ], window.innerWidth, window.innerHeight, "#ff0000", "#00ff00");
 
 //Scale canvas to window size for every 500ms
